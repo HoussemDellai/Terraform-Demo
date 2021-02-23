6de231b672fd1da328b0f575bf93e1771397a511
arash-malekzad <77361815+arash-malekzad@users.noreply.github.com>
2021-02-23 09:47:15 -0600

    Added provider

--- app-service-and-sql-database-var/main.tf
+++ app-service-and-sql-database-var/main.tf
@@ -1,3 +1,8 @@
+provider "azurerm" {
+  version = "=2.0.0"
+  features {}
+}
+
 resource "azurerm_resource_group" "RG-Terraform" {
   name     = "terraform-resource-group"
   location = "West Europe"
